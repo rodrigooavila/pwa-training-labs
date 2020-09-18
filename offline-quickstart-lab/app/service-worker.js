9897f50e8bf9adb794fb43941a196f003f3bc311
Rodrigo Avila <51334218+rodrigooavila@users.noreply.github.com>
2020-09-18 14:43:30 -0300

    Update service-worker.js

--- offline-quickstart-lab/app/service-worker.js
+++ offline-quickstart-lab/app/service-worker.js
@@ -1,1 +1,13 @@
-
+<script>
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(reg => {
+        console.log('Service worker registered! 😎', reg);
+      })
+      .catch(err => {
+        console.log('😥 Service worker registration failed: ', err);
+      });
+  });
+}
+</script>
